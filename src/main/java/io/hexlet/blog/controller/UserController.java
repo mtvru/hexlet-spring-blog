135520413bd0c9c1c80ba60637687c26c4416870
maksimus <maksimtsoi.nsk@gmail.com>
2026-03-17 15:13:53 +0600

    added ResponseStatus

--- src/main/java/io/hexlet/blog/controller/UserController.java
+++ src/main/java/io/hexlet/blog/controller/UserController.java
@@ -2,6 +2,7 @@ package io.hexlet.blog.controller;
 
 import io.hexlet.blog.model.User;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -32,6 +34,7 @@ public class UserController {
         return ResponseEntity.ok(result);
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public ResponseEntity<User> create(@Valid @RequestBody User user) {
         user.setId(nextId.getAndIncrement());
